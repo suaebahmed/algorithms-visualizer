30937e9d2bf02fa3aebb747471609f19c6feaad4
suaebahmed <suaebahmed12@gmail.com>
2022-09-10 01:49:24 +0600

    Updates

---
+++ precache-manifest.ef96f60fc8e80c3fffad5786ba5daa54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2162916596fb70adcd77",
+    "url": "/Algorithms-Visualizer/static/css/main.8fd8dd86.chunk.css"
+  },
+  {
+    "revision": "2162916596fb70adcd77",
+    "url": "/Algorithms-Visualizer/static/js/main.21629165.chunk.js"
+  },
+  {
+    "revision": "c31d7b36386fb6d90f27",
+    "url": "/Algorithms-Visualizer/static/js/1.c31d7b36.chunk.js"
+  },
+  {
+    "revision": "0de61d127b79ef59acd7",
+    "url": "/Algorithms-Visualizer/static/js/runtime~main.0de61d12.js"
+  },
+  {
+    "revision": "22453ad4384466ed0858eb109aa71bd3",
+    "url": "/Algorithms-Visualizer/static/media/path-finding.22453ad4.png"
+  },
+  {
+    "revision": "b32220b6203485e4fa83ded1ac94ed07",
+    "url": "/Algorithms-Visualizer/static/media/sort-algo.b32220b6.png"
+  },
+  {
+    "revision": "aa4f6636501fa9d9e6dd3f7f7b4afa39",
+    "url": "/Algorithms-Visualizer/static/media/prime-spiral.aa4f6636.png"
+  },
+  {
+    "revision": "050aff5122a19577248c7f8721a7242b",
+    "url": "/Algorithms-Visualizer/static/media/pin.050aff51.png"
+  },
+  {
+    "revision": "4cfe82ce70a806c1f60d44ea20fbabe6",
+    "url": "/Algorithms-Visualizer/static/media/goal.4cfe82ce.png"
+  },
+  {
+    "revision": "b79a075b5a82945008e801e711e90000",
+    "url": "/Algorithms-Visualizer/index.html"
+  }
+];
